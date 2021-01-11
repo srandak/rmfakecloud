82e4bf3ab462446e11b4805f0ce01a55f5664a0c
Ivan Krivulev <i.krivulev@gmail.com>
2021-01-11 21:54:54 +0100

    react login

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,37 +1,61 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react';
-import Nav from 'react-bootstrap/Nav'
-import Row from 'react-bootstrap/Row'
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import FileList from './components/FileList'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import React from 'react';
+// import Nav from 'react-bootstrap/Nav'
+// import Row from 'react-bootstrap/Row'
+// import Navbar from 'react-bootstrap/Navbar'
+// import Container from 'react-bootstrap/Container'
+// import FileList from './components/FileList'
+
+import React, { useEffect, useState } from 'react';
+import { Router, Route, Link } from 'react-router-dom';
+
+import { history, Role } from './_helpers';
+import { authenticationService } from './_services/authentication.service';
+
+import { PrivateRoute } from './_components/PrivateRoute';
+
+import { HomePage } from '../Components/Home/HomePage';
+import { AdminPage } from '../Components/Admin/AdminPage';
+import { LoginPage } from '../Components/Login/LoginPage';
 
 export default function App() {
+
+    const [authState, setAuthState] = useState({currentUser: authenticationService.currentUser, isAdmin: authenticationService.currentUser && authenticationService.currentUser.role === Role.Admin });
+
+    useEffect(() => {
+
+        authenticationService.currentUser.subscribe(x => setAuthState({currentUser: x, isAdmin: x && x.role === Role.Admin }))
+    }, []);
+
+    const logout = () => {
+        authenticationService.logout();
+        history.push('/login');
+    }
+
     return (
-        <>
-        <Navbar bg="dark" variant="dark" >
-            <Navbar.Brand href="#home">Stuff</Navbar.Brand>
-            <Nav activeKey="/home" onSelect={(k)=> console.log(k)} className="mr-auto" >
-            <Nav.Item>
-            <Nav.Link href="/home">Stuf</Nav.Link>
-            </Nav.Item>
-        </Nav>
-        </Navbar>
-        <Container fluid={true}>
-            <Row>
-            <Nav activeKey="/home" onSelect={(k)=> console.log(k)} className="flex-column" >
-                <Nav.Item>
-                    <Nav.Link href="/home">Active</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link href="/home">Active</Nav.Link>
-                </Nav.Item>
-            </Nav>
-            <div className="flex-column">
-                <FileList />
+        <Router history={ history }>
+        <div>
+            {authState.currentUser &&
+                <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <div className="navbar-nav">
+                        <Link to="/" className="nav-item nav-link">Home</Link>
+                        {authState.isAdmin && <Link to="/admin" className="nav-item nav-link">Admin</Link>}
+                        <a onClick={logout} className="nav-item nav-link">Logout</a>
+                    </div>
+                </nav>
+            }
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <PrivateRoute exact path="/" component={HomePage} />
+                            <PrivateRoute path="/admin" roles={[Role.Admin]} component={AdminPage} />
+                            <Route path="/login" component={LoginPage} />
+                        </div>
+                    </div>
+                </div>
             </div>
-        </Row>
-        </Container>
-        </>
+        </div>
+    </Router>
     );
 }
